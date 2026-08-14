ce70731d08da585c200f9f11864e97a3349bacb4
rafaelwebber <rafael.webber09@gmail.com>
2026-08-14 16:50:10 -0300

    fix: correacao da rota user para nao retornar a senha e add JWT nas senhas. Criacao rotas clientes

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,8 @@
+from datetime import datetime
 from flask import Flask, request, Blueprint, jsonify
+from sqlalchemy.engine import row
 from sqlalchemy.sql import text
-from models.usuarios import Usuario
+from werkzeug.security import generate_password_hash
 
 import db
 
@@ -11,15 +13,17 @@ bp = Blueprint("admin", __name__)
 @bp.get("/api/v1/admin/user")
 def listar_usuario():
     listar = db.SessionLocal()
+    try:
+        resultado= listar.execute(text(""" SELECT nome, role_id, cpf, rg, data_nascimento, email, telefone, telefone_secundario, create_data FROM usuarios
+        """))
+        usuarios = [dict(row) for row in resultado.mappings()]
 
-    usuarios = listar.get(Usuario, "nome")
-    if not usuarios:
-        return jsonify({"erro": "nao existe usuarios cadastrados"}), 404
-    else:
-        
-        return jsonify(usuarios)
-    
-
+        if not usuarios:
+            return jsonify({"erro": "nao existe usuarios cadastrados"}), 404
+        else:            
+            return jsonify(usuarios)
+    finally:
+        listar.close()
 
 @bp.post("/api/v1/admin/user")
 def create_usuario():
@@ -33,21 +37,21 @@ def create_usuario():
     telefone = payload.get("telefone")
     telefone_secundario = payload.get("telefone_secundario")
     senha = payload.get("senha")
-    create_data = payload.get("create_data")
-    update_data = payload.get("update_data")
+    create_data = datetime.now()
 
     if not nome or not cpf or not email or not senha or not create_data:
         return jsonify({"error": "campos obrgatorios"}), 400
     
     else:
+        senha_hash = generate_password_hash(senha)
         criar = db.SessionLocal()
         criar.execute(
             text(
         """ 
             INSERT INTO usuarios
-                (nome, role_id, cpf, rg, data_nascimento, email, telefone, telefone_secundario, senha, create_data, update_data ) 
+                (nome, role_id, cpf, rg, data_nascimento, email, telefone, telefone_secundario, senha, create_data) 
             VALUES (:nome, :role_id, :cpf, :rg, :data_nascimento, :email,
-            :telefone, :telefone_secundario, :senha, :create_data, :update_data)
+            :telefone, :telefone_secundario, :senha, :create_data)
         """
         ),
             {
@@ -59,11 +63,93 @@ def create_usuario():
             "email": email,
             "telefone": telefone,
             "telefone_secundario": telefone_secundario,
-            "senha": senha,
-            "create_data": create_data,
-            "update_data": update_data,
+            "senha": senha_hash,
+            "create_data": create_data,           
         },
         )
         criar.commit()
         criar.close()
         return jsonify({"mensagem": "usuario criado com sucesso!"}), 201
+
+@bp.post("/api/v1/admin/cliente")
+def create_cliente():
+    payload = request.get_json() or {}
+
+    nome = payload.get("nome")
+    nome_pai = payload.get("nome_pai")
+    nome_mae = payload.get("nome_mae")
+    estado_civil = payload.get("estado_civil")
+    cpf = payload.get("cpf")
+    rg = payload.get("rg")
+    data_nascimento = payload.get("data_nascimento")
+    idade = payload.get("idade")
+    alergico = payload.get("alergico")
+    observacao = payload.get("observacao")
+    email = payload.get("email")
+    telefone = payload.get("telefone")
+    telefone_secundario = payload.get("telefone_secundario")
+    cep = payload.get("cep")
+    uf = payload.get("uf")
+    cidade = payload.get("cidade")
+    bairro = payload.get("bairro")
+    rua = payload.get("rua")
+    numero = payload.get('numero')
+    create_data = datetime.now()
+
+    try:
+        if not nome or not email or not telefone:
+            return jsonify({"error": "campos obrigatorios"}), 400
+
+        else:
+            conn = db.SessionLocal()
+            conn.execute(text(
+                """
+                    INSERT INTO cliente (nome, nome_pai, nome_mae, estado_civil, cpf, rg, data_nascimento, idade, alergico, observacao, email, telefone, telefone_secundario, cep, uf, cidade, bairro, rua, numero, create_data)
+                VALUES(:nome, :nome_pai, :nome_mae, :estado_civil, :cpf, :rg, :data_nascimento, :idade, :alergico, :observacao, :email, :telefone, :telefone_secundario, :cep, :uf, :cidade, :bairro, :rua, :numero, :create_data)"""
+            ),
+            {
+                "nome": nome,
+                "nome_pai":nome_pai,
+                "nome_mae":nome_mae,
+                "estado_civil": estado_civil,
+                "cpf": cpf,
+                "rg": rg,
+                "data_nascimento": data_nascimento,
+                "idade": idade,
+                "alergico": alergico,
+                "observacao": observacao,
+                "email": email,
+                "telefone": telefone,
+                "telefone_secundario": telefone_secundario,
+                "cep": cep,
+                "uf": uf,
+                "cidade": cidade,
+                "bairro": bairro,
+                "rua": rua,
+                "numero": numero,
+                "create_data": create_data
+            },
+            )           
+            conn.commit()
+            conn.close()
+            return jsonify ({"mensage": "cliente criado com sucesso"}), 201
+    finally:
+
+        conn.close()
+
+@bp.get("/api/v1/admin/cliente")
+def listar_clientes():
+    conn = db.SessionLocal()
+
+    try:
+        resultado = conn.execute(text("""
+            SELECT * FROM cliente
+        """))
+        cliente = [dict(row) for row in resultado.mappings()]
+        if not cliente:
+            return jsonify({"error": "Nenhum cliente cadastrado"})
+        else:
+            return jsonify(cliente), 200
+
+    finally:
+        conn.close()
